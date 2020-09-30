9804d123b12c6d974ff0a4efdd6fbaf3e56a84db
Ivan Chistyakov <ivanchistyakov@BigMac.Dlink>
2020-09-30 20:24:00 +0300

    Game logic is implemented

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ class Player:
         self.chat_id = chat_id
         self.game = None
         self.balance = None
+        self.stake = None
     
     def receive_msg(self, text):
         bot.send_message(self.chat_id, text)
@@ -15,18 +16,120 @@ class Player:
 
 class Game:
     default_balance = 20
+    field_len = 5
 
     def __init__(self, player_a, player_b):
         self.players = [player_a, player_b]
         for player in self.players:
             player.balance = Game.default_balance
             player.game = self
+        self.position = Game.field_len // 2
         self.is_active = True
         self.declare_turn()
 
+    def get_player(self, player_id):
+        for player in self.players:
+            if player.id == player_id:
+                return player
+
+    def get_position_str(self, player_id):
+        if player_id == self.players[1].id:
+            pos = self.position
+        else:
+            pos = Game.field_len - self.position - 1 
+        
+        s = ["⚪️\n"] * Game.field_len
+        s[pos] = "🔴\n"
+        return "".join(s)
+
+    def get_opponent_str(self, player_id):
+        for player in self.players:
+            if player.id != player_id:
+                s = player.name + f" [💰{player.balance}]\n"
+                return s
+
+    def get_player_str(self, player_id):
+        for player in self.players:
+            if player.id == player_id:
+                s = f"You [💰{player.balance}]\n"
+                return s
+
+    def get_status_str(self, player):
+        s = self.get_opponent_str(player.id)
+        s += self.get_position_str(player.id)
+        s += self.get_player_str(player.id)
+        return s
+
+    def send_status(self):
+        for player in self.players:
+            player.stake = None
+            s = self.get_status_str(player)
+            player.receive_msg(s)
+
     def declare_turn(self):
         for player in self.players:
-            player.receive_msg("New game started!")
+            player.stake = None
+            s = self.get_status_str(player)
+            s += "\nLet's make a move!"
+            player.receive_msg(s)
+
+    def accept_stake(self, stake, player_id):
+        player = self.get_player(player_id)
+        if stake < 0:
+            player.receive_msg("Stake must be non-negative!")
+        elif player.balance < stake:
+            player.receive_msg("Not enough money!")
+        else:
+            player.stake = stake
+
+        # finish this phase
+        if self.players[0].stake is not None and \
+                self.players[1].stake is not None:
+            self.process_move()
+
+    def process_move(self):
+        if self.players[0].stake < self.players[1].stake:
+            self.position -= 1
+        elif self.players[0].stake > self.players[1].stake:
+            self.position += 1
+
+        for player in self.players:
+            player.balance -= player.stake
+            player.stake = None
+       
+        if self.position == 0:
+            self.send_status()
+            self.players[0].receive_msg("You lost!")
+            self.players[1].receive_msg("You won!")
+            self.finish_game()
+        if self.position == Game.field_len:
+            self.send_status()
+            self.players[0].receive_msg("You won!")
+            self.players[1].receive_msg("You lost!")
+            self.finish_game()
+        elif self.players[0].balance == 0 and \
+                self.players[1].balance >= self.position:
+            self.send_status()
+            self.players[0].receive_msg("You're out of money! You lost!")
+            self.players[1].receive_msg("You won!" +
+                    " Your opponent spent all the money.")
+            self.finish_game()
+        elif self.players[1].balance == 0 and \
+                self.players[0].balance >= Game.field_len - self.position - 1:
+            self.send_status()
+            self.players[1].receive_msg("You're out of money! You lost!")
+            self.players[0].receive_msg("You won!" +
+                    " Your opponent spent all the money.")
+            self.finish_game()
+        elif self.players[0].balance < Game.field_len - self.position - 1 and \
+                self.players[1].balance < self.position:
+            self.send_status()
+            msg = "It's a draw. No one has enough money to win."
+            self.players[0].receive_msg(msg)
+            self.players[1].receive_msg(msg)
+            self.finish_game()
+        else:
+            self.declare_turn()
 
     def surrender(self, player_id):
         self.is_active = False
@@ -34,3 +137,9 @@ class Game:
             player.game = None
             if player.id != player_id:
                 player.receive_msg("Opponent left.")
+
+    def finish_game(self):
+        for player in self.players:
+            player.game = None
+            player.balance = None
+
--- game_handler.py
+++ game_handler.py
@@ -39,3 +39,17 @@ class Game_handler:
                 Game(player_a, player_b)
             else:
                 Game_handler.players_queue.put(player_a)
+
+    @bot.message_handler(func=lambda message: True)
+    def make_stake(message):
+        """ Accept user's stake """
+        
+        player_id = message.from_user.id
+        player = Game_handler.players[player_id]
+        
+        if player.game is None:
+            bot.reply_to(message, "I don't understand your message.")
+            return
+
+        stake = int(message.text)
+        player.game.accept_stake(stake, player_id)
